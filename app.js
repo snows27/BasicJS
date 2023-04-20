6ba0c1252418ba93c83f4ff07c82b7c04430bbea
surze <surzepp@gmail.com>
2023-04-21 01:15:49 +0700

    modify JS to button then submit value

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-let score = parseInt(prompt("กรุณากรอกคะแนนของคุณ"));
-if(score >= 80){
+function getScore(){
+    let calScore = document.getElementById("score").value
+if(calScore >= 80){
     document.getElementById("result").innerHTML = "<b>คุณได้เกรด A</b>"
 }
-else if(score >= 60 && score < 80) {
+else if(calScore >= 60 && calScore < 80) {
     document.getElementById("result").innerHTML = "<b>คุณได้เกรด B</b>"
 }
-else if(score >= 40 && score < 60) {
+else if(calScore >= 40 && calScore < 60) {
     document.getElementById("result").innerHTML = "<b>คุณได้เกรด C</b>"
 }
-else if(score >= 20 && score < 40) {
+else if(calScore >= 20 && calScore < 40) {
     document.getElementById("result").innerHTML = "<b>คุณได้เกรด D</b>"
 }
 else {
     document.getElementById("result").innerHTML = "<b>คุณได้เกรด F</b>"
+}
 }
