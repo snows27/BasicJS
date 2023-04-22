892b6d9f575c08082fed75a4813c7a0c01ec251b
surze <surzepp@gmail.com>
2023-04-22 22:08:57 +0700

    Fahrenhait / Celcius Convertor

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-let round = parseInt(prompt("How many rounds do you want to play?"));
-let arrAns = ["HEAD", "TAIL"]
-let score = 0;
-for(i = 0; i < round; i++){
-let answer = prompt("Guess it!! 'Head' or 'Tail'");
-let randIndex = Math.floor(Math.random()*arrAns.length);
-let randAns = arrAns[randIndex]
-   let answerUP = answer.toUpperCase();
-   console.log(answerUP);
-   console.log(arrAns);
-    if(answerUP == randAns){
-        score++;
-        document.getElementById("result").innerHTML += "round: "+(i+1) + " You won" +"<br />" + "Your answer is: "+answer + "<br />" + "The answer is: "+randAns+"<br />"+"<br />";
-        alert("Correct")
-    }
-    else {
-        document.getElementById("result").innerHTML += "round: "+(i+1) + " You lost"+ "<br />" + "Your answer is: "+answer + "<br />" + "The answer is: "+randAns+"<br />"+"<br />";
-        alert("Miss")
-    }
+function fahcelConversion(){
+    let userInput = document.getElementById("userInput").value;
+    let celcius = (userInput - 32) * 5/9;
+    let fahrenheit = (userInput * 9/5) + 32;
+    //toFixed() is amount of decimal
+    document.getElementById("resultC").innerHTML = celcius.toFixed(2) + " °C";
+    document.getElementById("resultF").innerHTML = fahrenheit.toFixed(2) + " °F";
 }
-document.getElementById("score").innerHTML = "Your score is: " + score
