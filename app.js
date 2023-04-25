2891febfab28f4387795f3ff601843a46f3d3ffa
surze <surzepp@gmail.com>
2023-04-25 22:58:05 +0700

    fetch api and show/clear button

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-function fahcelConversion(r){
-    let rC = (r - 32) * 5/9;
-    let rF = (r * 9/5) + 32;
-    //toFixed() is amount of decimal
-    return {rC: rC.toFixed(2), rF: rF.toFixed(2)}
+const divuser = document.createElement("div");
+const divalbum = document.createElement("div");
+const Body = document.getElementById("showdata");
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then(user => {
+    user.map((x) => {
+        const p = document.createElement("p");
+        p.innerText = `${x.id}: ${x.username}`
+        divuser.append(p)
+        console.log(divuser);
+    }) 
+    console.log(user) 
+})
+
+function displayData(){
+   
+   Body.appendChild(divuser)
 }
-function displayTemperature(){
-    let temp = document.getElementById("userInput").value
-    let result = fahcelConversion(temp)
-    document.getElementById("resultC").innerHTML = result.rC + " °C"
-    document.getElementById("resultF").innerHTML = result.rF + " °F"
+function displayTitle(){
+   Body.appendChild(divalbum)
 }
+function clearData(){
+    divuser.remove()
+    divalbum.remove()
+}
+
+
+fetch('https://jsonplaceholder.typicode.com/albums')
+.then(res => res.json())
+.then(albums => {
+    albums.map((y) => {
+        const p = document.createElement("p")
+        p.innerText = `${y.userId}: ${y.title}`
+        divalbum.append(p)
+        console.log(y);
+    })
+})
