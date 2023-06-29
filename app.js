11f842b7e79247d90002a4c7b500996047bd98bb
surze <surzepp@gmail.com>
2023-06-29 16:46:51 +0700

    pushhhhhhh

--- app.js
+++ app.js
@@ -1,107 +1,28 @@
-//find max value
-var arr = [6, 1, -50, 200, 7, 9, -13, 20]
-var maxNum = arr[0] //index
+//Get 2 words to compare Anagram system.
+let firstVocab = prompt("Enter your 'first' vocabulary")
+let secondVocab = prompt("Enter your 'second' vocabulary")
 
-for (i=1; i<arr.length;i++){
-    if(arr[i] > maxNum){
-        maxNum = arr[i]
+//The logic of anagram is the frequency of each characters in the word should be the same.
+function anaGramWords(){
+    //split both words to the characters
+    let checkFV = [...firstVocab]
+    let checkSV = [...secondVocab]
+    const charCountFV = {}; // the object declare to store key and value {key: value}.
+    const charCountSV = {}; // the object declare to store key and value {key: value}.
+    //the loop will check by the key which is the character.
+    for (let i=0; i<checkFV.length; i++){ // let's say the word is "iceman" and "cinema".
+        const charFV = checkFV[i] // the result of charFV is: [i, c, e, m, a, n] in each iteration.
+        const charSV = checkSV[i] // the result of charSV is: [c, i, n, e, m, a] in each iteration.
+        charCountFV[charFV] = (charCountFV[charFV] || 0) + 1; // all of characters it is not the same so the key will not duplicate means all frequency of key will equal to 1.
+        charCountSV[charSV] = (charCountSV[charSV] || 0) + 1; // this step the same as above just different word.
+    }
+    //declare "char" to get each Characters per iteration
+    for (let char in charCountFV){
+    if (charCountFV[char] !== charCountSV[char]){
+        console.log("NOT Anagram");
+        return;
     }
 }
-console.log(`1. The Answer is The result is: ${maxNum}`);
-
-//number 1-100 if the number divisible by 3 print 'FIZZ'
-//             if the number divisible by 5 print 'Buzz'
-//             if the number divisible by 3,5 print 'FizzBuzz'
-var result = []
-for(i=1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-       let String = "fizzBuzz"
-       result.push(String)
-    }
-    else if(i % 5 === 0) {
-        let String = "Buzz"
-        result.push(String)
-    //    console.log("Buzz");
-    }
-    else if(i % 3 === 0){
-        let String = "Fizz"
-        result.push(String)
-    //    console.log("Fizz");
-    }
-    else {
-        result.push(i)
-        // console.log(i);
-    }
+        console.log("IS Anagram");
 }
-console.log("2. "+result.join(" | "));
-
-
-// Palindrome ? True: False
-let text = prompt("Type to check Palindrome")
-let textarr = text.split("").reverse().join("")
-if (text === textarr){
-    console.log(`3. the "${text}" is the Palindrome`);
-
-} else {
-    console.log(`3. the ${text} is not the Palindrome`);
-}
-
-//list the duplicate name
-let Firstlist = ["alice", "job", "trudy", "jack"]
-let Secondlist = ["janet", "alice", "james", "jack"]
-let duplicate = []
-for(i=0; i < Firstlist.length; i++){
-    for(j=0; j < Secondlist.length; j++){
-        if(Firstlist[i] === Secondlist[j]){
-            duplicate.push(Firstlist[i])
-        }
-    }
-}
-console.log("4. "+duplicate.join(" "));
-
-//the target number of fibonacci number 
-function fibonacci(n){
-    if(n <= 1) {
-        return n
-    }
-    return fibonacci(n-1) + fibonacci(n-2)
-}
-console.log(`5. The number sequence is: ${fibonacci(5)}`); 
-
-//in arrayNumber of [1 5 13 2 7 6] by adding in array equal to targetSum?
-const arrNum = [1, 5, 13, 2, 7, 6]
-const sumPrompt = parseInt(prompt("The targetSum you want"))
-for(i=0; i < arrNum.length; i++){
-    for(j=0; j < arrNum.length; j++){
-       if(arrNum[i] + arrNum[j] === sumPrompt){
-        console.log(`6. The sum ${sumPrompt} can be achieved by adding ${arrNum[i]} and ${arrNum[j]}`);
-        break;
-       } 
-       else {
-        console.log(`6. ${sumPrompt} is not in list of [${arrNum.join(" ")}] by adding together`);
-        break;
-       }
-    }
-}
-
-//Find the most frequently encountered words by given word
-const word = "This sentence has five words. Here are five more words. Five-word sentences are fine. But several together become monotonous"
-const wordCount = {}
-const wordFilter = word.toLowerCase().replace(/[.,-]/g, '').split(' ')
-console.log(wordFilter);
-for(i=0; i<wordFilter.length;i++){
-    if(wordCount[wordFilter[i]]){
-        wordCount[wordFilter[i]]++
-    } else {
-        wordCount[wordFilter[i]] = 1;
-    }
-}
-let maxCount = 0;
-let mostFreqWord = '';
-for(let word in wordCount){
-    if(wordCount[word] > maxCount){
-        maxCount = wordCount[word];
-        mostFreqWord = word;
-    }
-}
-console.log(`The most frequent word is "${mostFreqWord}" with a count of ${maxCount}`);
+anaGramWords();
